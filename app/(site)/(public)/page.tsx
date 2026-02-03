4e719406dcf37a507b86c493d25d4537e8909a1c
Lucas <lucaspy1943@icloud.com>
2026-02-03 19:11:06 +0100

    add dynamic page

--- app/(site)/(public)/page.tsx
+++ app/(site)/(public)/page.tsx
@@ -3,6 +3,8 @@ import HeroSection from "@/components/home/HeroSection";
 import InterestSection from "@/components/home/InterestSection";
 import SaleBanner from "@/components/home/SaleBanner";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col">
